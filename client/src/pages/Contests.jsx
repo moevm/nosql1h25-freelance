8d68bead11fdbad091a466fa6d6c34ad8a176ffa
qwers451 <mikhailzametaev@gmail.com>
2025-04-15 14:56:12 +0300

    fix: endByBar in contests

--- client/src/pages/Contests.jsx
+++ client/src/pages/Contests.jsx
@@ -6,7 +6,7 @@ import ContestsList from "../components/ContestsList.jsx";
 import {Context} from "../main.jsx";
 import { Link } from 'react-router-dom';
 import {CREATE_CONTEST_ROUTE} from "../utils/consts.js";
-import EndDateBar from "../components/EndByBar.jsx"; // Или NavLink, если нужно активное состояние
+import EndByBar from "../components/EndByBar.jsx"; // Или NavLink, если нужно активное состояние
 
 const Contests = () => {
     const { user } = useContext(Context);
@@ -24,7 +24,7 @@ const Contests = () => {
                     </Link>
                     <TypeBar />
                     <RewardBar />
-                    <EndDateBar />
+                    <EndByBar />
                 </Col>
                 <Col md={10}>
                     <ContestsList />
